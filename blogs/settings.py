5e87c17334e0c424bd71d64611c895cb0150ae57
MinHeinKoKo <kokominhein96@gmail.com>
2022-07-30 10:58:18 +0630

    blogs

--- blogs/settings.py
+++ blogs/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-lqy&)1j)atw(zmf+033q@5ra7_tk%6*mjd$bg^z45o8%8r16=j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -129,6 +129,7 @@ STATIC_URL = 'static/'
 MEDIA_ROOT= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
@@ -139,5 +140,16 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kokominhein96@gmail.com'
+EMAIL_HOST_PASSWORD = 'Heinminkoko76'
+EMAIL_USE_TLS = True
+#EMAIL_USE_SSL = False
 
 django_heroku.settings(locals())
--- vlog/views.py
+++ vlog/views.py
@@ -1,8 +1,12 @@
+import email
+from email import message
 from django.db.models import Q
 from turtle import title
 from django.shortcuts import get_object_or_404 , render , redirect
 from .models import Post ,Category
 from .forms import CommentForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def index(request):
@@ -37,3 +41,19 @@ def search(request):
     query = request.GET.get('query','')
     posts = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query))
     return render(request, 'search.html', {'posts':posts ,'query':query})
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        # send_mail(name, message, settings.EMAIL_HOST_USER,[email],fail_silently=False)
+        mail=send_mail(name, message, email, ['kokominhein96@gmail.com'])
+        mail.fail_silently = True
+        mail.send()
+
+
+        return render(request, 'index.html',{'name':name})
+
+    else:
+        return render(request,'index.html')
